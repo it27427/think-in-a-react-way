43c656b6e42c06f22fd93b4915a7d9764aba9918
it27427 <istiak.tushar.qtec@gmail.com>
2024-04-30 02:41:14 +0600

    import SignIn and SignUp Page in App

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 
 // IMPORT-PAGES
 import AboutPage from '@/pages/AboutPage';
+import BlogsPage from '@/pages/BlogsPage';
 import ContactPage from '@/pages/ContactPage';
 import ErrorPage from '@/pages/ErrorPage';
 import FaqPage from '@/pages/FaqPage';
 import HomePage from '@/pages/HomePage';
 import ProductsPage from '@/pages/ProductsPage';
-import BlogsPage from './pages/BlogsPage';
+
+// AUTH
+import SignInPage from '@/pages/auth/SignInPage';
+import SignUpPage from '@/pages/auth/SignUpPage';
 
 // IMPORT-COMPONENTS
 
@@ -22,6 +26,11 @@ const App = () => {
           <Route path='/contact' element={<ContactPage />} />
           <Route path='/faq' element={<FaqPage />} />
           <Route path='/products' element={<ProductsPage />} />
+
+          {/* AUTHENTICATION-PAGES */}
+          <Route path='/sign-in' element={<SignInPage />} />
+          <Route path='/sign-up' element={<SignUpPage />} />
+
           <Route path='/*' element={<ErrorPage />} />
         </Routes>
       </Router>
