c2b5fe0677b095a31adc5aa057dfea1fa2c00f24
it27427 <istiak.tushar.qtec@gmail.com>
2024-04-30 01:17:10 +0600

    main list done

--- src/components/MainList.jsx
+++ src/components/MainList.jsx
@@ -7,6 +7,21 @@ const MainList = () => {
       <li>
         <MenuLink linkPath={'/'} linkText='Home' />
       </li>
+      <li>
+        <MenuLink linkPath={'/about'} linkText='About' />
+      </li>
+      <li>
+        <MenuLink linkPath={'/blogs'} linkText='Blogs' />
+      </li>
+      <li>
+        <MenuLink linkPath={'/contact'} linkText='Contact' />
+      </li>
+      <li>
+        <MenuLink linkPath={'/products'} linkText='Products' />
+      </li>
+      <li>
+        <MenuLink linkPath={'/faq'} linkText='Faq' />
+      </li>
     </ul>
   );
 };
