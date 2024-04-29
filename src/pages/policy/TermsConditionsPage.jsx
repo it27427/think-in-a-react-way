aec6109d35a2551fd06e9a8e346e8e2ccae00b20
it27427 <istiak.tushar.qtec@gmail.com>
2024-04-30 03:27:28 +0600

    terms and conditions page added

---
+++ src/pages/policy/TermsConditionsPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TermsConditionsPage = () => {
+  return (
+    <div>
+      <h1>Terms And Conditions</h1>
+    </div>
+  );
+};
+
+export default TermsConditionsPage;
