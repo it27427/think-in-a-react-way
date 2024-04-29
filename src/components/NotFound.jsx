96ab30ab8218216c908deac29cde0cce3d8f6e19
it27427 <istiak.tushar.qtec@gmail.com>
2024-04-29 15:19:42 +0600

    not-found component and page completed

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,16 +1,27 @@
 import NotFoundThunmb from '@/assets/images/error/error.svg';
 import { Link } from 'react-router-dom';
+import { FaHome } from 'react-icons/fa';
 
 const NotFound = () => {
   return (
     <div className='flex items-center justify-center min-h-screen'>
-      <div className='flex flex-col items-center justify-center'>
-        <figure className='flex items-center justify-center w-40 h-40'>
+      <div className='flex flex-col items-center justify-center gap-4'>
+        <figure className='w-80 h-80'>
           <img src={NotFoundThunmb} alt='not-found-thumbnail' />
         </figure>
-        <Link to='/' className='bg-red-400'>
-          Back to Home
-        </Link>
+
+        <div className='flex flex-col items-center justify-center gap-4'>
+          <h2 className='text-3xl font-bold text-center text-red-600'>
+            Page Not Found!
+          </h2>
+          <Link
+            to='/'
+            className='bg-red-400 text-white flex items-center gap-2 rounded py-3 px-5 font-semibold transition hover:bg-red-700'
+          >
+            <FaHome />
+            Back to Home
+          </Link>
+        </div>
       </div>
     </div>
   );
