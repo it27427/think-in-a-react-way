5d8fd4053701fcb7d3f07025641ca34f763348e9
it27427 <istiak.tushar.qtec@gmail.com>
2024-04-30 01:31:59 +0600

    main import in home

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import MainSection from '@/components/layouts/MainSection';
 
 const HomePage = () => {
   return (
-    <>
+    <MainSection>
       <h1>Home Page</h1>
-    </>
+    </MainSection>
   );
 };
 
