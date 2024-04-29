9570d64fc2274b7761c6a8d9fb694c67ece1de46
it27427 <istiak.tushar.qtec@gmail.com>
2024-04-29 14:59:50 +0600

    all pages imported

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from '@/pages/HomePage';
+import AboutPage from '@/pages/AboutPage';
+import BlogsPage from './pages/BlogsPage';
+import ContactPage from '@/pages/ContactPage';
+import FaqPage from '@/pages/FaqPage';
+import ProductsPage from '@/pages/ProductsPage';
 
 const App = () => {
   return (
@@ -7,6 +12,11 @@ const App = () => {
       <Router>
         <Routes>
           <Route path='/' element={<HomePage />} />
+          <Route path='/about' element={<AboutPage />} />
+          <Route path='/blogs' element={<BlogsPage />} />
+          <Route path='/contact' element={<ContactPage />} />
+          <Route path='/faq' element={<FaqPage />} />
+          <Route path='/products' element={<ProductsPage />} />
         </Routes>
       </Router>
     </>
