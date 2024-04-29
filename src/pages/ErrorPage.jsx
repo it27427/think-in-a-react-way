b21f646863621ec3f23c66ee6cdc19dad1ac7a51
it27427 <istiak.tushar.qtec@gmail.com>
2024-04-30 02:36:11 +0600

    ErrorLayout added in ErrorPage

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,10 +1,11 @@
+import ErrorLayout from '@/components/layouts/ErrorLayout';
 import NotFound from '@/components/NotFound';
 
 const ErrorPage = () => {
   return (
-    <>
+    <ErrorLayout>
       <NotFound />
-    </>
+    </ErrorLayout>
   );
 };
 
