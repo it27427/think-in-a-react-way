f1cbad3462faeeb969d7bde5422683289250991d
it27427 <istiak.tushar.qtec@gmail.com>
2024-04-30 23:38:11 +0600

    inputfield component created

---
+++ src/components/InputField.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const InputField = ({ type, classes, id, placeholder, value, onChange }) => {
+  return (
+    <input
+      type={type}
+      className={`${classes}`}
+      id={`${id}`}
+      placeholder={`${placeholder}`}
+      value={`${value}`}
+      onChange={onChange}
+    />
+  );
+};
+
+export default InputField;
