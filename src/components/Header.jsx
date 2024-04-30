6ea8dcc24a5e4754e365957b19368956f8191623
it27427 <istiak.tushar.qtec@gmail.com>
2024-04-30 17:20:37 +0600

    header-bg changed and logo updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import Navbar from './Navbar';
 
 const Header = () => {
   return (
-    <header className='bg-teal-700 sticky top-0 z-50'>
+    <header className='bg-lime-200 sticky top-0 z-50'>
       <div className='container'>
         <Navbar />
       </div>
--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,12 +1,11 @@
-import logo from '@/assets/images/logo/react.svg';
+import logo from '@/assets/images/logo/logo.svg';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
-const Logo = ({ linkPath, logoText = 'ReactBlog' }) => {
+const Logo = ({ linkPath }) => {
   return (
-    <Link to={linkPath} className='flex items-center gap-2'>
-      <img src={logo} alt='brand-logo' width={40} height={40} />
-      <span className='text-slate-100 font-bold uppercase'>{logoText}</span>
+    <Link to={linkPath} className='flex items-center'>
+      <img src={logo} alt='brand-logo' width={128} height={40} />
     </Link>
   );
 };
