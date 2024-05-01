6091748d4d7a885536c68b17eb7768d8b280fe46
it27427 <istiak.tushar.qtec@gmail.com>
2024-05-01 13:38:25 +0600

    SignUpForm Created

--- src/pages/auth/SignUpPage.jsx
+++ src/pages/auth/SignUpPage.jsx
@@ -1,9 +1,22 @@
 import EmptyLayout from '@/layouts/EmptyLayout';
 
+import signInThumb from '@/assets/images/svg/login.svg';
+
 const SignUpPage = () => {
   return (
     <EmptyLayout>
-      <h1>Sign Up</h1>
+      <div className='flex flex-col lg:flex-row items-center'>
+        <div className='w-full lg:w-1/2 p-6 flex flex-col items-center justify-center gap-6'>
+          <h2 className='text-2xl uppercase text-center font-bold text-teal-500'>
+            Sign In
+          </h2>
+          <SignInForm />
+        </div>
+
+        <figure className='w-full lg:w-1/2 p-6'>
+          <img src={signInThumb} alt='sign-in' />
+        </figure>
+      </div>
     </EmptyLayout>
   );
 };
