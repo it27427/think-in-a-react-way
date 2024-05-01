7ad8525ff0d115bbafd9d677e7ee91f285d9ef25
it27427 <istiak.tushar.qtec@gmail.com>
2024-05-01 23:34:20 +0600

    get-categories

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,40 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
 import RootLayout from '@/layouts/RootLayout';
 
 import HeroSlider from '@/components/HeroSlider';
 import slides from '@/db/slider.json';
 
 const HomePage = () => {
+  const [categories, setCategories] = useState([]);
+  const [isError, setIsError] = useState('');
+
+  useEffect(() => {
+    const categories = async () => {
+      try {
+        const response = await axios.get(
+          'https://fakestoreapi.com/products/categories'
+        );
+        setCategories(response.data);
+      } catch (error) {
+        setIsError(error.message);
+      }
+    };
+
+    categories();
+  }, []);
+
   return (
     <RootLayout>
       <HeroSlider slides={slides} />
+
+      <ul>
+        {isError === '' && <h2>{isError}</h2>}
+        {categories.map((category) => (
+          <li key={category}>{category}</li>
+        ))}
+      </ul>
     </RootLayout>
   );
 };
