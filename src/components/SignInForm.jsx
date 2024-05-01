6794f13410f049bbf9eb1dab8fe4ac699aa2bc5a
it27427 <istiak.tushar.qtec@gmail.com>
2024-05-01 13:18:04 +0600

    password-field added

--- src/components/SignInForm.jsx
+++ src/components/SignInForm.jsx
@@ -7,15 +7,26 @@ const SignInForm = () => {
   const handleChange = (e) => setInputData(e.target.value);
 
   return (
-    <form className='px-6 w-full lg:px-12'>
-      <InputField
-        type='email'
-        classes='border border-slate-300 w-full h-10 py-3 px-4 rounded transition focus:border-slate-700'
-        id='email'
-        placeholder='Enter Email Address'
-        value={inputData}
-        onChange={handleChange}
-      />
+    <form className='px-6 w-full lg:px-12 flex flex-col gap-10'>
+      <div className='flex flex-col gap-5'>
+        <InputField
+          type='email'
+          classes='border border-slate-300 w-full h-10 py-3 px-4 rounded transition focus:border-slate-700'
+          id='email'
+          placeholder='Enter Email Address'
+          value={inputData}
+          onChange={handleChange}
+        />
+
+        <InputField
+          type='password'
+          classes='border border-slate-300 w-full h-10 py-3 px-4 rounded transition focus:border-slate-700'
+          id='password'
+          placeholder='Enter Password'
+          value={inputData}
+          onChange={handleChange}
+        />
+      </div>
     </form>
   );
 };
