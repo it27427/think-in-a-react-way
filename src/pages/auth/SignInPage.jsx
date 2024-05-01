8b2ce7f388e65d24f305e63a4b2f2a64429f30e3
it27427 <istiak.tushar.qtec@gmail.com>
2024-05-01 13:32:41 +0600

    title-text-color

--- src/pages/auth/SignInPage.jsx
+++ src/pages/auth/SignInPage.jsx
@@ -8,7 +8,9 @@ const SignInPage = () => {
     <EmptyLayout>
       <div className='flex flex-col lg:flex-row items-center'>
         <div className='w-full lg:w-1/2 p-6 flex flex-col items-center justify-center gap-6'>
-          <h2 className='text-2xl uppercase text-center font-bold'>Sign In</h2>
+          <h2 className='text-2xl uppercase text-center font-bold text-teal-500'>
+            Sign In
+          </h2>
           <SignInForm />
         </div>
 
